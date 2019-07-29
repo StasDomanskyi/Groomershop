fef7378ad2216439a2e39a45b7209faac0ddbd1e
Stanislav Domanskyi <Stanislav_Domanskyi@epam.com>
2019-07-29 15:29:20 +0300

    second slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "./index.pug"
 import  "./index.sass"
 import "../assets/img"
+
+
+import'../src/slider'
+import "../owl/owl.carousel.min.css"
+import $ from "jquery"
+import "../owl/owl.carousel.js"
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
