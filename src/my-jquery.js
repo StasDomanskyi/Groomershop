556af87be881f1f71e837ac1c36797071d3442e3
StXt <stas.domanskiy@gmail.com>
2019-07-30 13:09:44 +0300

    jquery slider created

--- src/my-jquery.js
+++ src/my-jquery.js
@@ -1,2 +1,49 @@
 import $ from "jquery"
 
+$(function() {
+    let counter = 0;
+    $('.right').click(function () {
+        if (counter > 2) {
+            counter = 2;
+        }
+        
+        if (counter === 2) {
+            $('.clients__inner').animate({
+                left: '-270vw'
+            },"slow");
+        }
+
+        if (counter === 1) {
+            $('.clients__inner').animate({
+                left: '-182vw'
+            },"slow");
+            counter++;
+        }
+
+        if (counter === 0) {
+            $('.clients__inner').animate({
+                left: '-93vw'
+            },"slow");
+            counter++;
+        }             
+    });
+
+
+    $('.left').click(function () {
+        if (counter === 2) {
+            $('.clients__inner').animate({
+                left: '-182vw'
+            },"slow");
+            counter--;
+        } else if (counter === 1) {
+            $('.clients__inner').animate({
+                left: '-93vw'
+            },"slow");
+            counter--;
+        } else if (counter === 0) {
+            $('.clients__inner').animate({
+                left: '-4rem'
+            },"slow");
+        }           
+    });
+})
