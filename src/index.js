7fd4610f397025884a700c26f55cb357fc5b018d
StXt <stas.domanskiy@gmail.com>
2019-07-30 12:05:30 +0300

    clients

--- src/index.js
+++ src/index.js
@@ -8,3 +8,11 @@ import "./components/header"
 import "./components/slider"
 import "./components/footer"
 import "./components/clients"
+import "./my-jquery.js"
+
+/* let rightArrow = document.querySelector('.right');
+let leftArrow = document.querySelector('.left');
+
+function moveRight() {
+    let container = document.querySelector('clients-inner')
+} */
